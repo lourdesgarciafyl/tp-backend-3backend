21177f217f07f57125c6743cfaaf88c561499952
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-10 13:27:40 -0300

    se agrega middlewares y ruta principal en indexjs

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+//database
+//rutas
+
+
+dotenv.config()
+
+const app = express()
+app.set("PORT", process.env.PORT || 4007);
+app.listen(app.get("PORT"), () =>{
+    console.log("Estoy en el puerto "+ app.get("PORT"))
+});
+
+//middlewares
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(morgan("dev")); 
+
+console.log(path.join(__dirname, `/public`));
+app.use(express.static(path.join(__dirname, `/public`)))
+
+// rutas
+// http://localhost:4007/paletacolores/colores
+app.use("/paletacolores")
